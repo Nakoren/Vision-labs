2bdcd9a66e5727234d220cf7339867f7907bb0c9
Nyakoren <minenkov_vladimir2003@inbox.ru>
2024-11-25 21:47:05 +0300

    Lab 3. Task 2
    Added normalisation.

--- Lab 1/Lab 3/Task.py
+++ Lab 1/Lab 3/Task.py
@@ -1,7 +1,6 @@
 import math
 import cv2
 import numpy as np
-
 '''
 cv2.namedWindow('NormalWindow', cv2.WINDOW_NORMAL)
 video = cv2.VideoCapture("C:/Users/minen/Desktop/Unik/Vision/random.mp4")
@@ -17,19 +16,33 @@ while (True):
 
 
 def get_gaussian_matrix(size, smooth_value):
-    av_deviation = (size, size)
-    result_matrix = np.ones((size, size), np.float32)
-    print(result_matrix[0,0])
+    av_deviation = (math.ceil(size), math.ceil(size))
+    result_matrix = np.ones((size, size), np.float64)
     for x in range(size):
         for y in range(size):
-            power = -1*(((x-av_deviation[0]) ^ 2 + (y-av_deviation[1]) ^ 2)/2*(smooth_value ^ 2))
+            power = -1*(((x-av_deviation[0]) ^ 2 + (y-av_deviation[1]) ^ 2)/2*(pow(smooth_value,2)))
 
-            result_matrix[x, y] = pow(1/(2*math.pi*(smooth_value ^ 2))*math.e, power)
+            result_matrix[x, y] = pow(1/(2*math.pi*(pow(smooth_value,2)))*math.e, power)
     return result_matrix
 
 
-size = 3
+def normalize(matr, size):
+    sum = 0
+    for line in matr:
+        for el in line:
+            sum+=el
+    result_matrix = np.ones((size, size), np.float64)
+    for i in range(len(matr)):
+       for j in range(len(matr)):
+           result_matrix[i,j] = matr[i,j]/sum
+
+    return result_matrix
+
+
+size = 5
+
 matr = get_gaussian_matrix(size, 1)
-print(matr)
+new_matr = normalize(matr, size)
+print(new_matr)
 
 
