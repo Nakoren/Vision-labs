e2eb1b43eb3c4290f477bcdadd9a1ec93a5e23e8
Nyakoren <minenkov_vladimir2003@inbox.ru>
2024-11-28 12:51:30 +0300

    Lab 3. Task 3
    Added hand-made gaussian blur. Added compressed testing pic (resason: too long processing for original 4500/3000)

--- Lab 1/Lab 3/Task.py
+++ Lab 1/Lab 3/Task.py
@@ -3,16 +3,29 @@ import cv2
 import numpy as np
 
 def start():
-    cv2.namedWindow('NormalWindow', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('Blurred', cv2.WINDOW_NORMAL)
-    img = cv2.imread("Pic.jpg",cv2.IMREAD_GRAYSCALE)
+
+    img = cv2.imread("Pic_compressed.jpg", cv2.IMREAD_GRAYSCALE)
+
+    cv2.namedWindow('Normal', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Normal', 1000, 500)
+    cv2.imshow("Normal", img)
+
 
     size = 5
+    blurred = gaussianBlur(img, size)
+    
+    cv2.namedWindow('Blurred', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Blurred', 1000, 500)
+    cv2.imshow("Blurred", blurred)
 
+    cv2.waitKey(0)
+    cv2.destroyWindow('Normal')
 
 
 def gaussianBlur(img, core_size):
+    print("Getting matrix")
     gaussian_matr = get_gaussian_matrix(core_size, 1)
+    print("normalizing")
     normalized_matr = normalize(gaussian_matr, core_size)
 
     blur_img = img.copy()
@@ -20,12 +33,14 @@ def gaussianBlur(img, core_size):
     center = core_size // 2
     for i in range(center, blur_img.shape[0] - center):
         for j in range(center, blur_img.shape[1] - center):
+            #print("Updating pixel" + str(i) +" - " + str(j))
             # операция свёртки
             new_value = 0
             for k in range(-(core_size // 2), core_size // 2 + 1):
                 for l in range(-(core_size // 2), core_size // 2 + 1):
                     new_value += img[i + k, j + l] * normalized_matr[k + (core_size // 2), l + (core_size // 2)]
             blur_img[i, j] = new_value
+    return blur_img
 
 
 def get_gaussian_matrix(size, smooth_value):
@@ -50,3 +65,5 @@ def normalize(matr, size):
            result_matrix[i,j] = matr[i,j]/sum
 
     return result_matrix
+
+start()
