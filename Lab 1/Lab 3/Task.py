847fe3521fbc8ef26d37bbad3faff7d2ec94d38a
Nyakoren <minenkov_vladimir2003@inbox.ru>
2024-11-28 13:25:13 +0300

    Lab 3
    Refactoring

--- Lab 1/Lab 3/Task.py
+++ Lab 1/Lab 3/Task.py
@@ -10,14 +10,17 @@ def start():
     cv2.resizeWindow('Normal', 1000, 500)
     cv2.imshow("Normal", img)
 
-    size = 5
-    smooth_value = 1
-    blurred = gaussianBlurCV(img, size, smooth_value)
+    size1 = 5
+    smooth_value1 = 1
+
+    size2 = 10
+    smooth_value2 = 3
+
+    blurred = gaussianBlur(img, size1, smooth_value1)
     
     cv2.namedWindow('Blurred', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Blurred', 1000, 500)
     cv2.imshow("Blurred", blurred)
-
     cv2.waitKey(0)
     cv2.destroyWindow('Normal')
 
@@ -30,13 +33,13 @@ def gaussianBlur(img, core_size, smooth_value):
     print("Getting matrix")
     gaussian_matr = get_gaussian_matrix(core_size, smooth_value)
     print("normalizing")
-    normalized_matr = normalize(gaussian_matr, core_size)
+    normalized_matr = normalize(gaussian_matr)
 
     blur_img = img.copy()
 
-    center = core_size // 2
-    for i in range(center, blur_img.shape[0] - center):
-        for j in range(center, blur_img.shape[1] - center):
+    core_center = core_size // 2
+    for i in range(core_center, blur_img.shape[0]):
+        for j in range(core_center, blur_img.shape[1] - core_center):
             #print("Updating pixel" + str(i) +" - " + str(j))
             # операция свёртки
             new_value = 0
@@ -54,16 +57,16 @@ def get_gaussian_matrix(size, smooth_value):
         for y in range(size):
             power = -1*(((x-av_deviation[0]) ^ 2 + (y-av_deviation[1]) ^ 2)/2*(pow(smooth_value,2)))
 
-            result_matrix[x, y] = 1/(2*math.pi*(pow(smooth_value,2)))* pow(math.e, power)
+            result_matrix[x, y] = 1/(2*math.pi*(pow(smooth_value,2))) * pow(math.e, power)
     return result_matrix
 
 
-def normalize(matr, size):
+def normalize(matr):
     sum = 0
     for line in matr:
         for el in line:
             sum+=el
-    result_matrix = np.ones((size, size), np.float64)
+    result_matrix = np.ones((matr.shape[0], matr.shape[1]), np.float64)
     for i in range(len(matr)):
        for j in range(len(matr)):
            result_matrix[i,j] = matr[i,j]/sum
